01b655908baff11bc4fed10eabe2fe6a30d1bb01
ltr <akambampati@hcf.com.au>
2023-09-11 16:14:02 +0530

    Added diff., pages, configured routing

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <h2>
+        Hunger games is all about facilitating users with multiple cuisines,
+        reachable on the click of a button.
+      </h2>
+    </div>
+  );
+};
+
+export default About;
--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -1,12 +1,13 @@
 import { Header } from "./Header";
 import Body from "./Body";
+import { Outlet } from "react-router-dom";
 
 export const AppLayout = () => {
   console.log("Virtual DOM", <Body />);
   return (
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
     </div>
   );
 };
