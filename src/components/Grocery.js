e5e4166e02832c7ebd932322756c77ba8614e057
ltr <apuroopgupca007@gmail.com>
2023-09-15 11:06:35 +0530

    Created Grocery feature

---
+++ src/components/Grocery.js
@@ -0,0 +1,10 @@
+const Grocery = () => {
+  return (
+    <h1>
+      Grocery Online store with lots of products ranging from vegetables, fruits
+      and groceries.
+    </h1>
+  );
+};
+
+export default Grocery;
