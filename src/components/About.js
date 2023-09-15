56123239ad6ade6ef4ac74fc09197dd21f663f77
ltr <apuroopgupca007@gmail.com>
2023-09-15 19:34:57 +0530

    Added About page with User component

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,25 @@
-const About = () => {
-  return (
-    <div>
-      <h1>About</h1>
-      <h2>
-        Hunger games is all about facilitating users with multiple cuisines,
-        reachable on the click of a button.
-      </h2>
-    </div>
-  );
-};
+import UserClass from "./UserClass";
+import { Component } from "react";
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return (
+      <div>
+        <h1>About</h1>
+        <h2>
+          Hunger games is all about facilitating users with multiple cuisines,
+          reachable on the click of a button.
+        </h2>
+        <User name="Apuroop" location="Hyderabad" />
+      </div>
+    );
+  }
+}
 
 export default About;
