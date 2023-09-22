3bf6a0813ad77ffee0f0913e7031cf6850042bae
ltr <apuroopgupca007@gmail.com>
2023-09-22 20:45:59 +0530

    Added Cart Slice to add/update/delete items from/into cart

---
+++ src/utils/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addItem: (state, action) => {
+      // mutating the state
+      state.items.push(action.payload);
+    },
+    removeItem: (state) => {
+      state.items.pop();
+    },
+    clearCart: (state) => {
+      console.log(state);
+      console.log(current(state));
+      state.items = [];
+      //   state = [] - This will not work
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export default cartSlice.reducer;
