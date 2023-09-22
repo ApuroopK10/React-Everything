a1b981f3888b9c969fd8446ce0c657dbb4e5c952
ltr <apuroopgupca007@gmail.com>
2023-09-22 20:45:16 +0530

    Added Redux and RTK

--- src/components/AppLayout.js
+++ src/components/AppLayout.js
@@ -3,15 +3,19 @@ import Body from "./Body";
 import { Outlet } from "react-router-dom";
 import UserContext from "../utils/userContext";
 import { useState } from "react";
+import { Provider } from "react-redux";
+import appStore from "../utils/appStore";
 
 export const AppLayout = () => {
   const [userName, setUserName] = useState("Global User");
   return (
-    <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
-      <div className="app">
-        <Header />
-        <Outlet />
-      </div>
-    </UserContext.Provider>
+    <Provider store={appStore}>
+      <UserContext.Provider value={{ loggedInUser: userName, setUserName }}>
+        <div className="app">
+          <Header />
+          <Outlet />
+        </div>
+      </UserContext.Provider>
+    </Provider>
   );
 };
