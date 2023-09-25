0ec4fe63d252aa4d0755c12af776b118a960401b
ltr <apuroopgupca007@gmail.com>
2023-09-25 18:47:38 +0530

    Added Testcases for Header cmp

---
+++ src/components/__tests__/header.test.js
@@ -0,0 +1,48 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Header } from "../Header";
+import { Provider } from "react-redux";
+import appStore from "../../utils/appStore";
+import { BrowserRouter } from "react-router-dom";
+import "@testing-library/jest-dom";
+
+it("should load render Header component with a Login button", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginBtn = screen.getByRole("button", { name: "Login" });
+  expect(loginBtn).toBeInTheDocument();
+});
+
+it("should load render Header component with cart info", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginBtn = screen.getByText(/Cart/);
+  expect(loginBtn).toBeInTheDocument();
+});
+
+it("should toggle Login-Logout status on click", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginBtn = screen.getByRole("button", { name: "Login" });
+  fireEvent.click(loginBtn);
+
+  const logoutBtn = screen.getByRole("button", { name: "Logout" });
+  expect(logoutBtn).toBeInTheDocument();
+});
