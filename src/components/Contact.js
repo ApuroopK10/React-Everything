8c8c7daa8751413d81182997ef427dd263009123
ltr <apuroopgupca007@gmail.com>
2023-09-25 18:18:27 +0530

    Written testcases for Contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,8 +2,23 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact us</h1>
+    <div className="text-red-700">
+      <h1 className="text-red-700 font-bold p-4 m-4">Contact us</h1>
+      <form>
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="name"
+        />
+        <input
+          type="text"
+          className="border border-black p-2 m-2"
+          placeholder="message"
+        />
+        <button className="border border-black p-2 m-2 rounded-lg bg-red-700 text-white font-bold">
+          Submit
+        </button>
+      </form>
     </div>
   );
 };
