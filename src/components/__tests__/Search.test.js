a1c6425354da19f83b69dec5304d46f22b3b26a0
ltr <apuroopgupca007@gmail.com>
2023-09-27 21:36:29 +0530

    wrapped testcases in describe method

--- src/components/__tests__/Search.test.js
+++ src/components/__tests__/Search.test.js
@@ -13,43 +13,45 @@ global.fetch = jest.fn(() => {
   });
 });
 
-it("should test Search functionality", async () => {
-  await act(async () =>
-    render(
-      <BrowserRouter>
-        <Body />
-      </BrowserRouter>
-    )
-  );
-  const cardsBeforeSearch = screen.getAllByTestId("restoCard");
-  expect(cardsBeforeSearch.length).toBe(20);
-  const searchBtn = screen.getByRole("button", { name: "Search" });
-  const searchInput = screen.getByTestId("searchInput");
-  fireEvent.change(searchInput, {
-    target: {
-      value: "KFC",
-    },
+describe("Should test search", () => {
+  it("should test Search functionality", async () => {
+    await act(async () =>
+      render(
+        <BrowserRouter>
+          <Body />
+        </BrowserRouter>
+      )
+    );
+    const cardsBeforeSearch = screen.getAllByTestId("restoCard");
+    expect(cardsBeforeSearch.length).toBe(20);
+    const searchBtn = screen.getByRole("button", { name: "Search" });
+    const searchInput = screen.getByTestId("searchInput");
+    fireEvent.change(searchInput, {
+      target: {
+        value: "KFC",
+      },
+    });
+    fireEvent.click(searchBtn);
+    const cardsAfterSearch = screen.getAllByTestId("restoCard");
+
+    expect(searchBtn).toBeInTheDocument();
+    expect(cardsAfterSearch.length).toBe(1);
   });
-  fireEvent.click(searchBtn);
-  const cardsAfterSearch = screen.getAllByTestId("restoCard");
 
-  expect(searchBtn).toBeInTheDocument();
-  expect(cardsAfterSearch.length).toBe(1);
-});
+  it("should test Top Rated functionality", async () => {
+    await act(async () =>
+      render(
+        <BrowserRouter>
+          <Body />
+        </BrowserRouter>
+      )
+    );
+    const cardsBeforeBtnClick = screen.getAllByTestId("restoCard");
+    expect(cardsBeforeBtnClick.length).toBe(20);
+    const topRatedBtn = screen.getByRole("button", { name: "Top Rated" });
+    fireEvent.click(topRatedBtn);
+    const cardsAfterBtnClick = screen.getAllByTestId("restoCard");
 
-it("should test Top Rated functionality", async () => {
-  await act(async () =>
-    render(
-      <BrowserRouter>
-        <Body />
-      </BrowserRouter>
-    )
-  );
-  const cardsBeforeBtnClick = screen.getAllByTestId("restoCard");
-  expect(cardsBeforeBtnClick.length).toBe(20);
-  const topRatedBtn = screen.getByRole("button", { name: "Top Rated" });
-  fireEvent.click(topRatedBtn);
-  const cardsAfterBtnClick = screen.getAllByTestId("restoCard");
-
-  expect(cardsAfterBtnClick.length).toBe(13);
+    expect(cardsAfterBtnClick.length).toBe(13);
+  });
 });
